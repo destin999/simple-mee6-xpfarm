d8a171bf0837d0c8901cec2acc6041e3cc8f9c12
MathiasTopKek <44008063+MathiasTopKek@users.noreply.github.com>
2021-01-27 21:26:14 +0200

    Update xpfarm.js

--- xpfarm.js
+++ xpfarm.js
@@ -8,18 +8,18 @@ function sleep(ms){
 }
 
 async function sendAndDelete(clnt) {
-  var g = clnt.guilds.get("SERVER ID");
-  var c = g.channels.find(chan => chan.name === "CHANNEL NAME");
+  var g = clnt.guilds.get("SERVER ID"); // guild id
+  var c = g.channels.find(chan => chan.name === "CHANNEL NAME"); // channel name
   
-  var msg = await c.send("Farming message here.");
-  await sleep(5000);
+  var msg = await c.send("Farming message here."); // message you want to be sent
+  await sleep(5000); // time to wait before deleting the message that was sent
   msg.delete();
 }
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  setInterval(async function(){await sendAndDelete(client);}, 65000);
+  setInterval(async function(){await sendAndDelete(client);}, 65000); // time for sending the message in miliseconds, right now it's 65 seconds you can edit it. 
 });
 
-client.login('TOKEN HERE');
+client.login('TOKEN HERE'); // user token
